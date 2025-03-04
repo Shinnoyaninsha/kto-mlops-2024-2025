a2011572e7de04bc4871eaff2a9e50a26c0c2709
Anthony Eric Gueib Hamann <122115874+Shinnoyaninsha@users.noreply.github.com>
2025-03-04 15:39:25 +0000

    feat : fastapi : get health & post pic

---
+++ cats_dogs_other/api/src/index.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, UploadFile
+import io
+from kto.inference import Inference
+
+app = FastAPI()
+model = Inference("./cats_dogs_other/api/resources/final_model.keras")
+
+
+@app.get("/health")
+def health():
+    return {"status": "OK"}
+
+@app.post("/upload")
+async def upload(file: UploadFile):
+    file_readed = await file.read()
+    file_bytes = io.BytesIO(file_readed)
+    return model.execute(file_bytes)
