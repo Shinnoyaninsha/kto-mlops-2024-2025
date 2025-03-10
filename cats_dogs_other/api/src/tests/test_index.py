a96f364626a0c384a3eed449a1247565245df000
Anthony Eric Gueib Hamann <122115874+Shinnoyaninsha@users.noreply.github.com>
2025-03-10 14:29:53 +0000

    feat: OAuth2

--- cats_dogs_other/api/src/tests/test_index.py
+++ cats_dogs_other/api/src/tests/test_index.py
@@ -3,7 +3,16 @@ import unittest
 from fastapi.testclient import TestClient
 from cats_dogs_other.api.src import index
 
+
+def skip_oauth():
+    return {}
+
+index.skip_oidc = True
+index.app.dependency_overrides[index.oauth2_scheme] = skip_oauth
 client = TestClient(index.app)
+
+index.skip_oidc = True
+index.app.dependency_overrides[index.oauth2_scheme] = skip_oauth
 
 
 class TestIndex(unittest.TestCase):
